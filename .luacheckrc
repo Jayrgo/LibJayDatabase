5bbb50cf061bc920b53876af56029bb1672915a0
jayrgo <jayrgo@users.noreply.github.com>
2020-02-21 20:08:52 +0100

    Namepsaces added

---
+++ .luacheckrc
@@ -0,0 +1,22 @@
+std = "lua51"
+max_line_length = false
+unused = false
+exclude_files = {
+    ".luacheckrc",
+}
+globals = {
+    "LibStub",
+    "UnitName",
+    "UnitClass",
+    "UnitRace",
+    "UnitFactionGroup",
+    "GetRealmName",
+    "date",
+    "tostringall",
+    "tContains",
+    "format",
+    "IsAddOnLoaded",
+    "CopyTable",
+    "wipe",
+    "GetServerTime",
+}
--- LibJayDatabase.lua
+++ LibJayDatabase.lua
@@ -1,5 +1,5 @@
 local MAJOR = "LibJayDatabase"
-local MINOR = 1
+local MINOR = 2
 
 assert(LibStub, format("%s requires LibStub.", MAJOR))
 
@@ -71,8 +71,7 @@ local function Restore(self)
         profiles = _G[self.variableName].profiles
         globals = _G[self.variableName].globals
 
-        self.lastChangeTime = _G[self.variableName].lastChangeTime or
-                                  GetServerTime()
+        self.lastChangeTime = _G[self.variableName].lastChangeTime or GetServerTime()
         self.lastChangeChar = _G[self.variableName].lastChangeChar or CHAR_KEY
 
         wipe(_G[self.variableName])
@@ -85,14 +84,10 @@ local function Restore(self)
     if type(globals) ~= "table" then globals = {} end
 
     for char, key in pairs(profileKeys) do
-        if type(char) == "string" and type(key) == "string" then
-            self.profileKeys[char] = key
-        end
+        if type(char) == "string" and type(key) == "string" then self.profileKeys[char] = key end
     end
     for key, data in pairs(profiles) do
-        if type(key) == "string" and type(data) == "table" then
-            self.profiles[key] = CopyTable(data)
-        end
+        if type(key) == "string" and type(data) == "table" then self.profiles[key] = CopyTable(data) end
     end
     for k, v in pairs(globals) do
         if type(k) == "string" then
@@ -107,14 +102,13 @@ local function Restore(self)
     self.restored = true
 
     self:SetProfile(self.profileKeys[CHAR_KEY] or self.defaultProfile)
-    self.callbacks:TriggerEvent("Loaded", self)
 end
 
 local LJEvent = LibStub("LibJayEvent")
 ---@param self table
----@param addOnName string
-local function ADDON_LOADED(self, addOnName)
-    if addOnName == self.addOnName then
+---@param addonName string
+local function ADDON_LOADED(self, addonName)
+    if addonName == self.addonName then
         LJEvent:Unregister("ADDON_LOADED", ADDON_LOADED, self)
 
         Restore(self)
@@ -136,7 +130,7 @@ function DatabaseMixin:OnLoad()
 
     LJEvent:Register("PLAYER_LOGOUT", PLAYER_LOGOUT, self)
 
-    local loaded, finished = IsAddOnLoaded(self.addOnName)
+    local loaded, finished = IsAddOnLoaded(self.addonName)
     if not loaded or not finished then
         LJEvent:Register("ADDON_LOADED", ADDON_LOADED, self)
     else
@@ -166,8 +160,32 @@ end
 ---@param event string
 local function TriggerEvent(self, event, ...)
     local callbacks = self.callbacks
+    --@debug@
+    callbacks:xTriggerEvent(event, self, ...)
+    callbacks:xTriggerEvent("OnChanged", self)
+    --@end-debug@
+    --[===[@non-debug@
     callbacks:TriggerEvent(event, self, ...)
     callbacks:TriggerEvent("OnChanged", self)
+    --@end-non-debug@]===]
+end
+
+---@param self table
+---@param event string
+local function TriggerEventOnNamepsaces(self, event, ...)
+    if self.namespaces then
+        for k, v in pairs(self.namespaces) do
+            local callbacks = v.callbacks
+            --@debug@
+            callbacks:xTriggerEvent(event, v, ...)
+            callbacks:xTriggerEvent("OnChanged", v)
+            --@end-debug@
+            --[===[@non-debug@
+            callbacks:TriggerEvent(event, self, ...)
+            callbacks:TriggerEvent("OnChanged", self)
+            --@end-non-debug@]===]
+        end
+    end
 end
 
 local error = error
@@ -175,9 +193,8 @@ local format = format
 ---@param profileKey string
 function DatabaseMixin:SetProfile(profileKey)
     if type(profileKey) ~= "string" then
-        error(format(
-                  "Usage: Database:SetProfile(profileKey): 'profileKey' - string expected got %s",
-                  type(profileKey), 2))
+        error(format("Usage: Database:SetProfile(profileKey): 'profileKey' - string expected got %s", type(profileKey),
+                     2))
     end
 
     if self.profile and self.profile == profileKey then return end
@@ -192,27 +209,26 @@ function DatabaseMixin:SetProfile(profileKey)
     if self.profile then setLastChange(self) end
     self.profile = profileKey
     TriggerEvent(self, "OnProfileChanged", profileKey)
+    TriggerEventOnNamepsaces(self, "OnProfileChanged", profileKey)
 end
 
 ---@param profileKey string
 function DatabaseMixin:ResetProfile(profileKey)
     if type(profileKey) ~= "string" then
-        error(format(
-                  "Usage: Database:SetProfile(profileKey): 'profileKey' - string expected got %s",
-                  type(profileKey), 2))
+        error(format("Usage: Database:ResetProfile(profileKey): 'profileKey' - string expected got %s",
+                     type(profileKey), 2))
     end
 
     if self.profiles[profileKey] then
         wipe(self.profiles[profileKey])
         self.profiles[profileKey] = CopyTable(self.defaults)
-        if self.profile == profileKey then
-            self.db = self.profiles[profileKey]
-        end
+        if self.profile == profileKey then self.db = self.profiles[profileKey] end
     end
 
     setLastChange(self)
 
     TriggerEvent(self, "OnProfileReset", profileKey)
+    TriggerEventOnNamepsaces(self, "OnProfileReset", profileKey)
 end
 
 ---@return string profile
@@ -230,9 +246,7 @@ local tContains = tContains
 function DatabaseMixin:GetUsedProfiles()
     local usedProfiles = {}
     for _, key in pairs(self.profileKeys) do
-        if not tContains(usedProfiles, key) then
-            usedProfiles[#usedProfiles + 1] = key
-        end
+        if not tContains(usedProfiles, key) then usedProfiles[#usedProfiles + 1] = key end
     end
     return usedProfiles
 end
@@ -242,9 +256,7 @@ function DatabaseMixin:GetUnusedProfiles()
     local usedProfiles = self:GetUsedProfiles()
     local unusedProfiles = {}
     for key in pairs(self.profiles) do
-        if not tContains(usedProfiles, key) then
-            unusedProfiles[#unusedProfiles + 1] = key
-        end
+        if not tContains(usedProfiles, key) then unusedProfiles[#unusedProfiles + 1] = key end
     end
     return unusedProfiles
 end
@@ -252,39 +264,32 @@ end
 ---@param profileKey string
 function DatabaseMixin:DeleteProfile(profileKey)
     if type(profileKey) ~= "string" then
-        error(format(
-                  "Usage: Database:DeleteProfile(profileKey): 'profileKey' - string expected got %s",
-                  type(profileKey), 2))
+        error(format("Usage: Database:DeleteProfile(profileKey): 'profileKey' - string expected got %s",
+                     type(profileKey), 2))
     end
 
     if self.profile == profileKey then
-        error(format(
-                  "Usage: Database:DeleteProfile(profileKey): 'profileKey' - cannot delete current profile (%s)",
-                  profileKey), 2)
+        error(format("Usage: Database:DeleteProfile(profileKey): 'profileKey' - cannot delete current profile (%s)",
+                     profileKey), 2)
     end
 
     self.profiles[profileKey] = nil
-    for char, key in pairs(self.profileKeys) do
-        if key == profileKey then self.profileKeys[char] = nil end
-    end
+    for char, key in pairs(self.profileKeys) do if key == profileKey then self.profileKeys[char] = nil end end
 
     setLastChange(self)
 
     TriggerEvent(self, "OnProfileDeleted", profileKey)
+    TriggerEventOnNamepsaces(self, "OnProfileDeleted", profileKey)
 end
 
 ---@param from string
 function DatabaseMixin:CopyProfile(from)
     if type(from) ~= "string" then
-        error(format(
-                  "Usage: Database:CopyProfile(from): 'from' - string expected got %s",
-                  type(from), 2))
+        error(format("Usage: Database:CopyProfile(from): 'from' - string expected got %s", type(from), 2))
     end
 
     if not self.profiles[from] then
-        error(format(
-                  "Usage: Database:CopyProfile(from): 'from' - cannot find profile (%s)",
-                  from), 2)
+        error(format("Usage: Database:CopyProfile(from): 'from' - cannot find profile (%s)", from), 2)
     end
 
     local current = self:GetProfile()
@@ -302,6 +307,7 @@ function DatabaseMixin:CopyProfile(from)
     setLastChange(self)
 
     TriggerEvent(self, "OnProfileCopied", from)
+    TriggerEventOnNamepsaces(self, "OnProfileCopied", from)
 end
 
 local tostringall = tostringall
@@ -309,17 +315,14 @@ local function coercePath(...) return tostringall(...) end
 
 local validValueTypes = {"boolean", "number", "string", "nil"}
 for i = 1, #validValueTypes do validValueTypes[validValueTypes[i]] = true end
-local validValueTypesString = table.concat(validValueTypes, ", ", 1,
-                                           #validValueTypes - 1)
-validValueTypesString = format("%s or %s", validValueTypesString,
-                               validValueTypes[#validValueTypes])
+local validValueTypesString = table.concat(validValueTypes, ", ", 1, #validValueTypes - 1)
+validValueTypesString = format("%s or %s", validValueTypesString, validValueTypes[#validValueTypes])
 
 ---@param value any
 ---@param usage string
 local function checkValue(value, usage)
     if not validValueTypes[type(value)] then
-        error(format("Usage: Database:%s: 'value' - %s expected got %s", usage,
-                     validValueTypesString, type(value)), 3)
+        error(format("Usage: %s: 'value' - %s expected got %s", usage, validValueTypesString, type(value)), 3)
     end
 end
 
@@ -342,26 +345,24 @@ end
 ---@param value boolean | number | string | nil
 ---@param path any
 function DatabaseMixin:SetDefault(value, path, ...)
-    checkValue(value, "SetDefault(value, path[, ...])")
+    checkValue(value, "Database:SetDefault(value, path[, ...])")
 
     set(self.defaults, value, coercePath(path, ...))
 
-    if self.restored then
-        copyDefaults(self.profiles[self:GetProfile()], self.defaults)
-    end
+    if self.restored then copyDefaults(self.profiles[self:GetProfile()], self.defaults) end
 end
 
 ---@param value any
----@param path string
+---@param path any
 function DatabaseMixin:SetWithoutResponse(value, path, ...)
-    checkValue(value, "SetWithoutResponse(value, path[, ...]")
+    checkValue(value, "Database:SetWithoutResponse(value, path[, ...]")
     if set(self.db, value, coercePath(path, ...)) then setLastChange(self) end
 end
 
 ---@param value boolean | number | string | nil
 ---@param path string
 function DatabaseMixin:Set(value, path, ...)
-    checkValue(value, "Set(value, path[, ...]")
+    checkValue(value, "Database:Set(value, path[, ...]")
     if set(self.db, value, coercePath(path, ...)) then
         setLastChange(self)
         TriggerEvent(self, "OnValueChanged", value, coercePath(path, ...))
@@ -386,19 +387,16 @@ function DatabaseMixin:Get(...)
     return get(self.defaults, coercePath(...))
 end
 
-function DatabaseMixin:Reset(...)
-    self:Set(get(self.defaults, coercePath(...)), coercePath(...))
-end
+function DatabaseMixin:Reset(...) self:Set(get(self.defaults, coercePath(...)), coercePath(...)) end
 
 ---@return any defult
-function DatabaseMixin:GetDefault(...) return
-    get(self.defaults, coercePath(...)) end
+function DatabaseMixin:GetDefault(...) return get(self.defaults, coercePath(...)) end
 
 local LAST_CHANGE_FORMAT = "|cffcfcfcf%s (%s)|r"
 local date = date
 function DatabaseMixin:GetLastChange()
-    return format(LAST_CHANGE_FORMAT, date("%Y-%m-%d %X", self.lastChangeTime),
-                  self.lastChangeChar), self.lastChangeTime, self.lastChangeChar
+    return format(LAST_CHANGE_FORMAT, date("%Y-%m-%d %X", self.lastChangeTime), self.lastChangeChar),
+           self.lastChangeTime, self.lastChangeChar
 end
 
 function DatabaseMixin:Clear()
@@ -412,19 +410,17 @@ end
 ---@param value boolean | number | string | nil
 ---@param path string
 function DatabaseMixin:SetGlobalDefault(value, path, ...)
-    checkValue(value, "SetGlobalDefault(value, path[, ...]")
+    checkValue(value, "Database:SetGlobalDefault(value, path[, ...]")
 
     set(self.defaults_globals, value, coercePath(path, ...))
 end
 
-function DatabaseMixin:GetGlobalDefault(...)
-    get(self.defaults_globals, coercePath(...))
-end
+function DatabaseMixin:GetGlobalDefault(...) get(self.defaults_globals, coercePath(...)) end
 
 ---@param value boolean | number | string | nil
 ---@param path string
 function DatabaseMixin:SetGlobal(value, path, ...)
-    checkValue(value, "SetGlobal(value, path[, ...]")
+    checkValue(value, "Database:SetGlobal(value, path[, ...]")
 
     if set(self.globals, value, coercePath(path, ...)) then
         setLastChange(self)
@@ -441,6 +437,78 @@ function DatabaseMixin:GetGlobal(...)
     return get(self.defaults_globals, coercePath(...))
 end
 
+---@return boolean isRestored
+function DatabaseMixin:IsRestored()
+    return self.restored
+end
+
+local NamespaceMixin = {}
+
+function NamespaceMixin:OnLoad()
+    self.OnLoad = nil
+    self.callbacks = self.callbacks or LJCallback:New(self)
+end
+
+---@param value boolean | number | string | nil
+---@param path any
+function NamespaceMixin:SetDefault(value, path, ...)
+    checkValue(value, "Namespace:SetDefault(value, path[, ...])")
+
+    local parent = self.parent
+
+    set(parent.defaults, value, "namespaces", self.key, coercePath(path, ...))
+
+    if parent.restored then copyDefaults(parent.profiles[parent:GetProfile()], parent.defaults) end
+end
+
+---@return any defult
+function NamespaceMixin:GetDefault(...) return get(self.parent.defaults, "namespaces", self.key, coercePath(...)) end
+
+---@param value boolean | number | string | nil
+---@param path string
+function NamespaceMixin:Set(value, path, ...)
+    checkValue(value, "Namespace:Set(value, path[, ...]")
+
+    if set(self.parent.db, value, "namespaces", self.key, coercePath(path, ...)) then
+        setLastChange(self.parent)
+        TriggerEvent(self, "OnValueChanged", value, coercePath(path, ...))
+    end
+end
+
+---@param value any
+---@param path any
+function NamespaceMixin:SetWithoutResponse(value, path, ...)
+    checkValue(value, "NamespaceMixin:SetWithoutResponse(value, path[, ...]")
+    local parent = self.parent
+    if set(parent.db, value, "namespaces", self.key, coercePath(path, ...)) then setLastChange(parent) end
+end
+
+---@return boolean | number | string | nil value
+function NamespaceMixin:Get(...)
+    if self.parent.restored then
+        local value = get(self.parent.db, "namespaces", self.key, coercePath(...))
+        if type(value) ~= "nil" then return value end
+    end
+    return get(self.parent.defaults, "namespaces", self.key, coercePath(...))
+end
+
+local LJMixin = LibStub("LibJayMixin")
+---@param key string
+function DatabaseMixin:NewNamespace(key)
+    if type(key) ~= "string" then
+        error(format("Usage: %s:NewNamespace(key): 'key' - string expected got %s.", MAJOR, type(key), 2))
+    end
+
+    if self.namespaces and self.namespaces[key] then
+        error(format("Usage: %s:NewNamespace(key): 'key' Namespace '%s' already exists.", MAJOR, key), 2)
+    end
+
+    local namespace = LJMixin:Mixin({key = key, parent = self}, NamespaceMixin)
+    self.namespaces = self.namespaces or {}
+    self.namespaces[key] = namespace
+    return namespace
+end
+
 local DEFAULT_PROFILES = {
     REALM = GetRealmName(),
     FACTION = select(2, UnitFactionGroup("player")),
@@ -448,30 +516,28 @@ local DEFAULT_PROFILES = {
     CLASS = UnitClass("player")
 }
 
-local LJMixin = LibStub("LibJayMixin")
----@param addOnName string
+---@param addonName string
 ---@param variableName string
 ---@param defaultProfile string | "\"REALM\"" | "\"FACTION\"" | "\"RACE\"" | "\"CLASS\""
-function lib:New(addOnName, variableName, defaultProfile)
-    if type(addOnName) ~= "string" then
-        error(format(
-                  "Usage: %s:New(addOnName, variableName[, defaultProfile]): 'addOnName' - string expected got %s",
-                  MAJOR, type(addOnName), 2))
+function lib:New(addonName, variableName, defaultProfile)
+    if type(addonName) ~= "string" then
+        error(format("Usage: %s:New(addonName, variableName[, defaultProfile]): 'addonName' - string expected got %s",
+                     MAJOR, type(addonName), 2))
     end
     if type(variableName) ~= "string" then
         error(format(
-                  "Usage: %s:New(addOnName, variableName[, defaultProfile]): 'variableName' - string expected got %s",
+                  "Usage: %s:New(addonName, variableName[, defaultProfile]): 'variableName' - string expected got %s",
                   MAJOR, type(variableName), 2))
     end
     defaultProfile = defaultProfile or CHAR_KEY
     if type(defaultProfile) ~= "string" then
         error(format(
-                  "Usage: %s:New(addOnName, variableName[, defaultProfile]): 'defaultProfile' - string expected got %s",
+                  "Usage: %s:New(addonName, variableName[, defaultProfile]): 'defaultProfile' - string expected got %s",
                   MAJOR, type(defaultProfile), 2))
     end
 
     local database = LJMixin:Mixin({
-        addOnName = addOnName,
+        addonName = addonName,
         variableName = variableName,
         defaultProfile = DEFAULT_PROFILES[defaultProfile] or defaultProfile
     }, DatabaseMixin)
